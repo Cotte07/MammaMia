dc27ed9b583dfbfff6844f6ac6faecf70f8d656e
Cotte07 <satinoscar13@gmail.com>
2024-11-11 21:32:52 -0500

    cuarto commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { MammaMiaComponent } from './mamma-mia/mamma-mia.component';
+import { CrearPizzaComponent } from './crearpizza/crearpizza.component';
 
 export const routes: Routes = [
-    {path: "", component: MammaMiaComponent}
+    {path: "", component: MammaMiaComponent},
+    {path: "pizza", component: CrearPizzaComponent}
 ];
--- src/app/crearpizza/crearpizza.component.ts
+++ src/app/crearpizza/crearpizza.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-crearpizza',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './crearpizza.component.html',
   styleUrl: './crearpizza.component.css'
 })
-export class CrearpizzaComponent {
+
+export class CrearPizzaComponent   {
 
 }
